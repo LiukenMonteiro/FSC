6620cacaa930c73c8aa8372eb3883b06fd0affbe
LiukenMonteiro <liuken37@gmail.com>
2025-09-29 09:21:46 -0300

    mais código assíncrono

--- 02_nodejs-assícrono/03_funções-callback/index.js
+++ 02_nodejs-assícrono/03_funções-callback/index.js
@@ -18,12 +18,24 @@ const getUserVideos = (onSucess, onError) => {
     }, 2000);
 };
 
+const getUserVideosDetails = (onSucess, onError) => {
+    setTimeout(() => {
+        onSucess({ id: 1, name: "NOde.js Assíncrono", duration: 1});
+    }, 2000);
+};
+
 const user = loginUser("test@test.com", "password", (user) => {
     console.log("Usuário logado com sucesso!");
     console.log(user);
 
     getUserVideos((videos) => {
         console.log("Videos do usuário recuperados com sucesso!", videos);     
+
+        getUserVideosDetails((video) => {
+            console.log("Detalhes do vídeo 1 do usuário:", video);
+        }, (error) => {
+            console.log(error);
+        });
     }, (error) => {
         console.log(error);
     })
